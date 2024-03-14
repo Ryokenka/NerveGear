15075846dfe9dfad899723a94d06317da7f4c687
Thomas Ploix <thomas.ploix@epfedu.fr>
2024-03-14 14:35:45 +0100

    Feature ADD fonctionnalité minecraft Avancé avec bouton interface

--- Interface/GameEngine/MinecraftTest.py
+++ Interface/GameEngine/MinecraftTest.py
@@ -1,23 +1,26 @@
 from time import sleep
 import pyautogui as pt
 
+class MinecraftEngine : 
+    def __init__(self):
+        print("Init Minecraft")
+
+    def bouger_perso(self, key_press, duration, action='walking'):
+        print("Bouger Perso")
+        sleep(2)
+        pt.keyDown(key_press)
+        if action == 'walking':
+            print('walking')
+        elif action == 'attack':
+            pt.keyDown('x')
+
+        sleep(duration)
+        pt.keyUp('x')
+        pt.keyUp(key_press)
 
 
+#duration = 10
 
-def bouger_perso(key_press,duration,action='walking'):
-    pt.keyDown(key_press)
-    if action == 'walking':
-        print('walking')
-    elif action=='attack':
-        pt.keyDown('x')
-    
-    sleep(duration)
-    pt.keyUp('x')
-    pt.keyUp(key_press)
-
-
-duration = 10
-
-while duration !=0:
-    bouger_perso('z',2,'attack')
-    duration -= 1
+#while duration !=0:
+#    bouger_perso('z',2,'attack')
+#    duration -= 1
--- Interface/WindowInterface/HomeInterface.py
+++ Interface/WindowInterface/HomeInterface.py
@@ -1,12 +1,14 @@
 from tkinter import *
 
+from Interface.GameEngine.MinecraftTest import MinecraftEngine
 
 
-
-
-#Création de la première fenètre 
+MC = MinecraftEngine()
+def MCbouge():
+    MC.bouger_perso('z', 2, 'attack')
+#Création de la première fenètre
 fenetreMain =Tk()
-fenetreMain.geometry('500x500')
+fenetreMain.geometry('720x480')
 fenetreMain.title("Interface Test")
 fenetreMain['bg']='ivory'
 fenetreMain.resizable(height=True,width=True)
@@ -14,7 +16,7 @@ fenetreMain.minsize(height="480",width="360")
 
 labelMain = Label(fenetreMain, text="Interface de configuration",background='ivory', font=("Courrier",30)).pack(side=TOP,padx=50)
 
-#Création du Menu
+#Création du Menu 
 menuMain = Menu(fenetreMain)
 fichier = Menu(menuMain,tearoff=0)
 fichier.add_command(label="Jeu")
@@ -23,9 +25,16 @@ configuration = Menu(menuMain,tearoff=0)
 configuration.add_command(label="Enregistrer la configuration")
 
 menuMain.add_cascade(label="Fichier",menu=fichier)
-menuMain.add_cascade(label="Configuration",menu=configuration)
-
-
-
+menuMain.add_cascade(label="Configuration", menu=configuration)
 fenetreMain.config(menu=menuMain)
+
+#Création de la frame
+
+frameBd = Frame(fenetreMain, bg="ivory")
+btn = Button(frameBd,text='Avancer',font=("Courrier", 25), bg='#96C3CE', fg='black',
+             command=MCbouge)
+btn.pack(pady=25,fill=X)
+
+
+frameBd.pack(expand=YES)
 fenetreMain.mainloop()
