dda6d05b7ca98f56734348cb57703a24bd399423
Fabrikot <fabien.devillechabrolle@epfedu.fr>
2024-03-14 18:22:59 +0100

    Feature modif design & implémentation multi frames
    Feature modif design & implémentation multi pages

--- Interface/WindowInterface/HomeInterface.py
+++ Interface/WindowInterface/HomeInterface.py
@@ -16,42 +16,50 @@ def MCdroite():
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-
-        self.geometry('720x480')
+        self.geometry('1080x720')
         self.title("Interface Test")
         self.iconbitmap("../Image/LOGO_NVG.ico")
-        self.grid_columnconfigure(0, weight=1)
+
+        self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.labelMain = (ctk.CTkLabel(self, text="Interface de configuration", font=("Courrier", 20))
-                          .grid(row=0,column=1, padx=20, pady=20))
-        # Create CTkFrame
-        #menuMain = Menu(self, background="red")
-        #fichier = Menu(menuMain, tearoff=0, background="red")
-        #fichier.add_command(label="Jeu")
 
-        #configuration = Menu(menuMain, tearoff=0)
-        #configuration.add_command(label="Enregistrer la configuration")
-
-        #menuMain.add_cascade(label="Fichier", menu=fichier)
-        #menuMain.add_cascade(label="Configuration", menu=configuration)
-        #self.config(menu=menuMain)
         self.frame_navig = FrameNavig(self)
-        self.frame_navig.grid(column=0, padx=20, pady=20)
+        self.frame_navig.grid(row=0,column=0, padx=20, pady=20, sticky="ns")
+        self.frame_navig.grid_rowconfigure(0, weight=1)
+        self.frame_navig.configure(width=140, corner_radius=0)
 
-        self.frame_boutons = FrameBoutons(self)
-        self.frame_boutons.grid(row=1, column=1, padx=20, pady=20)
+        self.mainframe = ctk.CTkFrame(self)
+
+        self.labelMain = (ctk.CTkLabel(self.mainframe, text="Interface de configuration", font=("Courrier", 20))
+                          .grid(row=0, padx=20, pady=20))
+        container = ctk.CTkFrame(self.mainframe)
+        container.grid(row=1, padx=20, pady=20)
+
+        self.mainframe.grid(row=0,column=1, padx=20, pady=20, sticky="nsew")
+
+        self.frames = {}
+
+        for F in (FrameBoutons, FrameConfig):
+            self.frames[F] = F(container)
+            self.frames[F].grid(row=0, column=0, sticky="nsew")
+        self.show_frame(FrameBoutons)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 
 class FrameNavig(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-        self.button1 = ctk.CTkButton(self, text="Configuration", command=MCavancer)
-        self.button1.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        self.button1 = ctk.CTkButton(self, text="Configuration", command=lambda: master.show_frame(FrameConfig))
+        self.button1.pack(pady=20, padx=20)
 
-        self.button2 = ctk.CTkButton(self, text="Test Commandes", command=MCreculer)
-        self.button2.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        self.button2 = ctk.CTkButton(self, text="Test Commandes", command=lambda: master.show_frame(FrameBoutons))
+        self.button2.pack(pady=20, padx=20)
+
 class FrameBoutons(ctk.CTkFrame):
-    def __init__(self,master):
-        super().__init__(master)
+    def __init__(self, master):
+        ctk.CTkFrame.__init__(self, master)
         self.button1 = ctk.CTkButton(self, text="Avancer", command=MCavancer)
         self.button1.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
 
@@ -63,6 +71,12 @@ class FrameBoutons(ctk.CTkFrame):
 
         self.button4 = ctk.CTkButton(self, text="Droite", command=MCdroite)
         self.button4.grid(row=1, column=2, padx=10, pady=10, sticky="ew")
+
+class FrameConfig(ctk.CTkFrame):
+    def __init__(self, master):
+        ctk.CTkFrame.__init__(self, master)
+        self.button1 = ctk.CTkButton(self, text="Configurer")
+        self.button1.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
 
 if __name__ == "__main__":
     app = App()
