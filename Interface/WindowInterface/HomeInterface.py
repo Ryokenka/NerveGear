7e6bb31f8a7ceaeedbead2d7f915eb1704e31f42
Fabrikot <fabien.devillechabrolle@epfedu.fr>
2024-03-19 19:42:35 +0100

    Feature ajout scrollable frame

--- Interface/WindowInterface/HomeInterface.py
+++ Interface/WindowInterface/HomeInterface.py
@@ -25,28 +25,29 @@ class App(ctk.CTk):
 
         self.frame_navig = FrameNavig(self)
         self.frame_navig.grid(row=0,column=0, padx=20, pady=20, sticky="ns")
-        self.frame_navig.grid_rowconfigure(0, weight=1)
-        self.frame_navig.configure(width=140, corner_radius=0)
 
         self.mainframe = ctk.CTkFrame(self)
 
         self.labelMain = (ctk.CTkLabel(self.mainframe, text="Interface de configuration", font=("Courrier", 20))
                           .grid(row=0, padx=20, pady=20))
         container = ctk.CTkFrame(self.mainframe)
-        container.grid(row=1, padx=20, pady=20)
+        container.grid(row=1, padx=20, pady=20, sticky="nsew")
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        self.mainframe.grid(row=0,column=1, padx=20, pady=20, sticky="nsew")
+        self.mainframe.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+        self.mainframe.grid_columnconfigure(0, weight=1)
+        self.mainframe.grid_rowconfigure(1, weight=1)
 
         self.frames = {}
 
-        for F in (FrameBoutons, FrameConfig):
+        for F in (FrameConfig, FrameBoutons):
             self.frames[F] = F(container)
             self.frames[F].grid(row=0, column=0, sticky="nsew")
-        self.show_frame(FrameBoutons)
+        self.show_frame(FrameConfig)
 
     def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
+        self.frames[cont].tkraise()
 
 class FrameNavig(ctk.CTkFrame):
     def __init__(self, master):
@@ -59,7 +60,11 @@ class FrameNavig(ctk.CTkFrame):
 
 class FrameBoutons(ctk.CTkFrame):
     def __init__(self, master):
-        ctk.CTkFrame.__init__(self, master)
+        super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+
         self.button1 = ctk.CTkButton(self, text="Avancer", command=MCavancer)
         self.button1.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
 
@@ -74,10 +79,44 @@ class FrameBoutons(ctk.CTkFrame):
 
 class FrameConfig(ctk.CTkFrame):
     def __init__(self, master):
+        def optionmenu_callback(choice):
+            print("optionmenu dropdown clicked:", choice)
+
         ctk.CTkFrame.__init__(self, master)
-        self.button1 = ctk.CTkButton(self, text="Configurer")
-        self.button1.grid(row=0, column=1, padx=10, pady=10, sticky="ew")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        list=ctk.CTkScrollableFrame(self)
+        list.grid(row=0, column=0, sticky="nsew")
+
+        for i in range(total_rows):
+            for j in range(2):
+                if j ==0:
+                    list.e=ctk.CTkLabel(list, text=lst[i][0])
+                else:
+                    optionmenu_var = ctk.StringVar(value=lst[i][1][0])
+                    list.e=ctk.CTkOptionMenu(list, values=lst[i][1],
+                                             command=optionmenu_callback,
+                                             variable=optionmenu_var)
+
+                list.e.grid(row=i, column=j, padx=20, pady=10)
+
+
 
 if __name__ == "__main__":
+    lst = [["Touche Avancer", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche Gauche", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche Droite", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche Reculer", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche Sauter", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche Action 1", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche Action 2", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche F1", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche F2", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche F3", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche F4", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche F5", ["Capteur EEG","Capteur ECG","Capteur EMG"]],
+           ["Touche F6", ["Capteur EEG","Capteur ECG","Capteur EMG"]],]
+
+    total_rows = len(lst)
     app = App()
     app.mainloop()
